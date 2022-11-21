df253d3cd956fbf30c32c1ec78b9bdad7a86b9cb
Andrey <andreas42r@yandex.ru>
2022-11-21 14:40:08 +0700

    feature/addCard: with temlate

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Верх-Чебула',
+    link: 'http://chebula.ru/attachments/Image/IMG_5997.JPG?template=generic'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Новороссийск',
+    link: 'https://img.pac.ru/landmarks/476692/big/61C538047F00010130DB5476CFDE465B.jpg'
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Натухаевская',
+    link: 'https://stroy-dom-anapa.ru/wp-content/uploads/2021/03/skola-natuhaevsk.jpg'
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Новосибирск',
+    link: 'https://mtdata.ru/u16/photoA19E/20015809021-0/original.jpg'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Кемерово',
+    link: 'https://img-fotki.yandex.ru/get/373511/30348152.23d/0_9345b_8255e9f_orig'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Мариинск',
+    link: 'https://static.tildacdn.com/tild3439-3063-4536-b130-333465363630/5ca456b282c94d548f4e.jpeg'
   }
 ];
 const editBtnEl = document.querySelector('.profile__btn-edit');
@@ -34,7 +34,7 @@ const addPopup = getPopup('popup_type_add-form');
 const imagePopup = getPopup('popup_type_image');
 
 /* Получение формы редактирования профиля и ее элементов */
-const editFormEl = document.querySelector('.form_type_edit');
+const editFormEl = editPopup.querySelector('.form_type_edit');
 const editFormNameEl = editFormEl.querySelector('.form__input_edit_name')
 const editFormJobEl = editFormEl.querySelector('.form__input_edit_job');
 const editFormBtnEl = editFormEl.querySelector('.form__btn_type_submit');
@@ -43,85 +43,57 @@ const editFormBtnEl = editFormEl.querySelector('.form__btn_type_submit');
 const profileNameEl = document.querySelector('.profile__title');
 const profileJobEl = document.querySelector('.profile__subtitle');
 
-/* Получение формы редактирования профиля и ее элементов */
-const addFormEl = document.querySelector('.form_type_add');
+/* Получение формы добавления карточки */
+const addFormEl = addPopup.querySelector('.form_type_add');
 const addFormNameEl = addFormEl.querySelector('.form__input_add_name')
 const addFormUrlEl = addFormEl.querySelector('.form__input_add_url');
 const addFormBtnEl = addFormEl.querySelector('.form__btn_type_submit');
 
-const cardsList = document.querySelector('.cards__list');
+const cardsList = document.querySelector('.cards__list'); /* <ul class="cards__list"></ul> */
+const cardTemplate = document.querySelector('#card-template').content; /* Это <li>...</li> */
 
 function initializeCards(initialCards) {
-  showCards(initialCards);
-
-  /* Добавляем события на показ изображения и удаление карточки */
-  let cards = cardsList.querySelectorAll('.cards__item');
-
-  cards.forEach((card, index) => {
-    let cardDelBtnEl = card.querySelector('.cards__delete');
-    let cardImageEl = card.querySelector('.cards__image');
-    let cardTitleEl = card.querySelector('.cards__title');
-    let cardLikeBtnEl = card.querySelector('.cards__like');
-
-    cardDelBtnEl.addEventListener('click', function(e) {
-      e.preventDefault();
-
-      delCard(index);
-      initializeCards(initialCards);
-    });
-
-    cardImageEl.addEventListener('click', function() {
-      setImagePopupValues(cardImageEl, cardTitleEl);
-      openPopup(imagePopup);
-    });
-
-    cardLikeBtnEl.addEventListener('click', function(e) {
-      e.preventDefault();
-
-      cardLikeBtnEl.classList.toggle('cards__like_active');
-    }); /* После добавления/удаления карточки, лайк не сохраняется, в ТЗ про это, вроде, не сказано, поэтому пока так */
+  initialCards.forEach(function( initialCard ) {
+    addCard(initialCard.name, initialCard.link);
   })
 }
 
-/* Показать карточки */
-function showCards(initialCards) {
-  cardsList.innerHTML = '';
-  initialCards.forEach((card) => {
-    let cardHTML = `
-      <li>
-        <article>
-          <div class="cards__item">
-            <button class="cards__delete" type="button" title="Удалить"></button>
-            <img class="cards__image" src="${card.link}" alt="${card.name}">
-            <div class="cards__content">
-              <h2 class="cards__title">${card.name}</h2>
-              <button class="cards__like" type="button" title="Поставить лайк"></button>
-            </div>
-          </div><!-- /.cards__item -->
-        </article>
-      </li>
-    `;
-    cardsList.insertAdjacentHTML('beforeend', cardHTML);
-  });
+function addCard( name, url ) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleEl = cardElement.querySelector('.cards__title');
+  const cardImageEl = cardElement.querySelector('.cards__image');
+  const cardDelButnEl = cardElement.querySelector('.cards__delete');
+  const cardLikeButnEl = cardElement.querySelector('.cards__like');
+
+  cardImageEl.src = url;
+  cardImageEl.alt = name;
+  cardTitleEl.textContent = name;
+
+  cardDelButnEl.addEventListener('click', function(e) {
+    const cardLiEl = e.target.closest('li');
+
+    delCard(cardLiEl);
+  })
+
+  cardImageEl.addEventListener('click', function() {
+    setImagePopupValues(name, url);
+    openPopup(imagePopup);
+  })
+
+  cardLikeButnEl.addEventListener('click', function() {
+    cardLikeButnEl.classList.toggle('cards__like_active');
+  })
+
+  cardsList.prepend(cardElement);
 }
 
-function addCard(name, url) {
-  initialCards.unshift({name: name, link: url});
-}
-
-function delCard(index) {
-  initialCards.splice(index, 1);
+function delCard(cardLiEl) {
+  cardLiEl.remove();
 }
 
 /* Получение конкретного попапа по классу модификатору*/
 function getPopup(popupClass) {
-  let popup;
-  popupEls.forEach( function(el) {
-    if ( el.classList.contains(popupClass)) {
-      popup = el;
-    }
-  });
-  return popup;
+  return Array.from(popupEls).find(popup => popup.classList.contains(popupClass)); /* До чего техника дошла! Преобразуем псевдомассив в массив, ищем попап с нужным классом*/
 }
 
 /* Функция сохранения формы редактирования профиля */
@@ -151,9 +123,13 @@ function setEditFormValues() {
 }
 
 /* Set image popup values */
-function setImagePopupValues(cardImage, cardTitle) {
-  imagePopup.querySelector('.popup__image').src = cardImage.src;
-  imagePopup.querySelector('.popup__title').textContent = cardTitle.textContent;
+function setImagePopupValues(name, url) {
+  const image = imagePopup.querySelector('.popup__image');
+  const title = imagePopup.querySelector('.popup__title');
+
+  image.src = url;
+  image.alt = name;
+  title.textContent = name;
 }
 /* ----------------------------------------------------------------------------------------------------- */
 
@@ -189,7 +165,6 @@ addFormEl.addEventListener('submit', function(e) {
   let url = addFormUrlEl.value;
 
   addCard(name, url);
-  initializeCards(initialCards);
   closePopup(addPopup);
 });
 
